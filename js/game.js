c451921159e04df54fd6fb842635ffc8e1e9143a
alejandro blanco <alejandro@alejandros-MacBook-Air-2.local>
2022-04-07 18:09:05 +0200

    several random obstacles and increased player speed

--- js/game.js
+++ js/game.js
@@ -1,90 +1,97 @@
-
-
-
 class Game {
+  constructor(create, draw) {
+    this.player = null;
+    this.create = create;
+    this.draw = draw;
+    this.objectArray = [];
+    this.obstacleCounter = 0;
 
-    constructor(create, draw){
-        this.player = null;
-        this.create = create;
-        this.draw = draw;
-        this.objectArray = [];
+    //this.obstacleDelete = obstacleDelete;
+  }
 
+  start() {
+    //  create and draw player
+    this.player = new Player();
+    this.player.domElement = this.create("player");
+    this.draw(this.player);
+
+    // move obstacle
+    let intervalId = setInterval(() => {
+      if (this.obstacleCounter === 30) {
+        this.obstacle = new Obstacle();
+        this.obstacle.domElement = this.create("obstacle");
+        console.log(this.obstacle.domElement);
+        this.draw(this.obstacle);
+
+        this.objectArray.push(this.obstacle);
+        this.obstacleCounter = Math.floor(Math.random() * 30);
+      }
+      this.objectArray.forEach((element) => {
+        if (element.positionY === 0) {
+            //removing elemen from array
+          this.objectArray.shift();
+
+          element.domElement.remove();
+        }
+        console.log(this.objectArray.length);
+
+
+        element.moveDown();
+        this.draw(element);
+      });
+      this.obstacleCounter++;
+    }, 50);
+  }
+
+  movePlayer(direction) {
+    if (direction === "left") {
+      this.player.moveLeft();
+    } else if (direction === "right") {
+      this.player.moveRight();
     }
-
-    start(){
-        //  create and draw player
-        this.player = new Player();
-        this.player.domElement = this.create("player");
-        this.draw(this.player);
-           
-          // move obstacle
-        setInterval(code, delay)
-
-
-
-
-          let objectInterval = setInterval(() => {
-            let objectNumber = 0;
-            this.obstacle = new Obstacle();
-            this.obstacle.domElement = this.create("obstacle");
-            this.draw(this.obstacle);
-            //we add every obstacle to obstacle array
-             this.objectArray.push(this.obstacle);
-         },3000)
-
-        let intervalId = setInterval(() => {
-            this.objectArray.forEach(element => {
-            element.moveDown();
-            this.draw(element);
-            })
-        },100)
-    }
-
-    movePlayer(direction){
-            if(direction === "left"){
-                this.player.moveLeft();
-            }else if (direction === "right"){
-                this.player.moveRight();
-            }
-            this.draw(this.player);
-    }
+    this.draw(this.player);
+  }
 }
 
 class Player {
-    constructor() {
-        this.positionX = 50;
-        this.positionY = 0;
-        this.domElement = null;
-    }
+  constructor() {
+    this.positionX = 50;
+    this.positionY = 0;
+    this.domElement = null;
+  }
 
-    moveLeft() {
-        this.positionX--;
-        console.log(`moving left ...${this.positionX}`)
-    }
+  moveLeft() {
+    this.positionX-= 5;
+    console.log(`moving left ...${this.positionX}`);
+  }
 
-    moveRight() {
-        this.positionX++;
-        console.log(`moving right ...${this.positionX}`)
-
-    }
-    
+  moveRight() {
+    this.positionX+= 5;
+    console.log(`moving right ...${this.positionX}`);
+  }
 }
 
 class Obstacle {
-    constructor(){
-       // this.eliminator = eliminator
-        this.positionX = 50;
-        this.positionY = 90;
-        this.domElement = null;
-    }
+  constructor() {
+    // this.eliminator = eliminator
+    this.positionX = Math.floor(Math.random() * 80);
+    this.positionY = 90;
+    this.domElement = null;
+  }
 
-    moveDown(){
-        this.positionY--;
+  moveDown() {
+    this.positionY--;
+  }
 
-    }
+  //   deleteObstacle(element){
+  //     // if(element.positionY === 0){
 
-    
+  //     //     //delete object from array
 
+  //     //     //
+  //     // }
+
+  //   }
 }
 
 //const myPlayer = new Player();
